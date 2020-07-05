0948280eed9b50858732a97e18312d425f12f183
kkumarta <67810906+kkumarta@users.noreply.github.com>
2020-07-05 11:34:31 -0400

    Update giggle.c

--- giggle.c
+++ giggle.c
@@ -6,5 +6,16 @@ int main(void) {
     printf("to check on the auto push build step\n");
     printf("to display the ouput in GH page\n");
     printf("to test the given scenarios");
+    int n = 9; 
+    printf("%d", fib(n)); 
+    getchar();
     return 0;
 }
+
+
+int fib(int n) 
+{ 
+    if (n <= 1) 
+        return n; 
+    return fib(n - 1) + fib(n - 2); 
+} 
