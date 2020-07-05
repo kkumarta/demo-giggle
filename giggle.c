5b5428b07e718c1f6f2d74897dfd6572dc054b7d
kkumarta <67810906+kkumarta@users.noreply.github.com>
2020-07-05 09:51:45 -0400

    Update giggle.c

--- giggle.c
+++ giggle.c
@@ -5,6 +5,6 @@ int main(void) {
     printf("demo from kishore.\n");
     printf("to check on the auto push build step\n");
     printf("to display the ouput in GH page");
-    printf("to test the scenarios");
+    printf("to test the given scenarios");
     return 0;
 }
